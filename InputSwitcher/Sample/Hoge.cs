ccdb35996c1f9b53d89dd89f71358dfa3a8cbc64
euglenach <midorimushiiii@gmail.com>
2020-02-09 14:41:11 +0900

    first

---
+++ InputSwitcher/Sample/Hoge.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace InputSwitcher{
+    public class Hoge : MonoBehaviour, IInputReceiver{
+        void Start(){
+            InputSw.GetKey(this, KeyCode.C)
+                   .Subscribe(_ => {Debug.Log("Hoge");});
+        }
+    }
+}
