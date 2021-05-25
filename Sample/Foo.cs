7e776d72ccd316b500070ee6c073ef2c26068d6c
Euglenach <midorimushiiii@gmail.com>
2021-05-26 04:49:23 +0900

    フォルダを移動

---
+++ Sample/Foo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace InputSwitcher{
+    public class Foo : MonoBehaviour, IInputReceiver{
+        void Start(){
+            InputSw.GetKey(this, KeyCode.C)
+                   .Subscribe(_ => {Debug.Log("Foo");});
+        }
+    }
+}
